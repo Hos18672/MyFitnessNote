4b9226e12ffc8ffa1be80243be7c93a3b5bedecc
hos18672@spengergasse.at <09109224375Mahdi>
2021-07-18 17:36:42 +0200

    add Date to the Workouts

--- app/src/main/java/com/example/myfitneesnote/MuskelGroupActivity.kt
+++ app/src/main/java/com/example/myfitneesnote/MuskelGroupActivity.kt
@@ -2,30 +2,42 @@ package com.example.myfitneesnote
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import kotlinx.android.synthetic.main.activity_muskel_group.*
 
 class MuskelGroupActivity : BaseActivity() {
+    var GymType: String? = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_muskel_group)
-        var GymType: String? = intent.getStringExtra("WorkoutType")
+        GymType= intent.getStringExtra("WorkoutType")
         muscle_txt.text = GymType
         fullscreen()
         setupActionBar()
         btnBack.setOnClickListener{onBackPressed()}
+        //Burst muscle
+        brust_btn.setOnClickListener   { setOnclickMuscle(brust_btn) }
+        //Biceps muscle
+        biceps_btn.setOnClickListener  { setOnclickMuscle(biceps_btn) }
+        //Triceps muscle
+        triceps_btn.setOnClickListener { setOnclickMuscle(triceps_btn) }
+        //Forarms muscle
+        forearms_btn.setOnClickListener{ setOnclickMuscle(forearms_btn) }
+        //Traps muscle
+        traps_btn.setOnClickListener   { setOnclickMuscle(traps_btn) }
+        //Upper-abs muscle
+        upperabs_btn.setOnClickListener{ setOnclickMuscle(upperabs_btn) }
+        //Lower-abs muscle
+        lowerabs_btn.setOnClickListener{ setOnclickMuscle(lowerabs_btn) }
+        //shoulder muscle
+        shoulder_btn.setOnClickListener{ setOnclickMuscle(shoulder_btn) }
+    }
 
-        brust_btn.setOnClickListener  {
-           var intent = Intent (this, AddWorkoutActivity::class.java)
-            intent.putExtra("MuskelName", brust_btn.text)
-            intent.putExtra("GymName",GymType)
-            startActivity(intent)
-        }
-        biceps_btn.setOnClickListener  {
-            var intent = Intent (this, AddWorkoutActivity::class.java)
-            intent.putExtra("MuskelName", biceps_btn.text)
-            intent.putExtra("GymName", GymType)
-                startActivity(intent)
-        }
+    private  fun setOnclickMuscle(btn : Button){
+        var intent = Intent (this, AddWorkoutActivity::class.java)
+        intent.putExtra("MuskelName", btn.text)
+        intent.putExtra("GymName", GymType)
+        startActivity(intent)
     }
     private fun setupActionBar() {
         setSupportActionBar(toolBar_muscle_gruppe_activity)
